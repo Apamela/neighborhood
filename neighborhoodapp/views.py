d4d4fa788405829988f77a6adb20e42e35eeb182
Apamela <pamelab.desire@gmail.com>
2019-11-06 05:29:56 +0200

    add the classes

--- neighborhoodapp/views.py
+++ neighborhoodapp/views.py
@@ -2,8 +2,36 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+import datetime
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect('signout')
+    else:
+        if request.user.id == 1:
+            if request.method == 'POST':
+                form = NeighborhoodForm(request.POST)
+                if form.is_valid():
+                    neighborhood = Neighborhood(neighborhood_name=request.POST['neighborhood_name'],neighborhood_location=request.POST['neighborhood_location'])
+                    neighborhood.save()
+                return redirect('index')
+            else:
+                form = NeighborhoodForm()
+            neighborhoods = Neighborhood.objects.all()
+            return render(request,'index.html',{'neighborhoods':neighborhoods,'form':form})
+        elif request.user != 1:
+            user = UserProfile.objects.filter(user = request.user).first()
+            if user is None:
+                user = UserProfile(user=request.user)
+                user.save()
+            if user.neighborhood is None:
+                title = 'Neighborhood'
+                neighborhoods = Neighborhood.objects.all()
+                return render(request,'index.html',{'neighborhoods':neighborhoods})
+            else:
+                user = UserProfile.objects.filter(user = request.user).first()
+                return redirect(reverse('neighborhood',args=[user.neighborhood.id]))
 
 def profile(request,user_id):
     user = User.objects.get(id = user_id)
